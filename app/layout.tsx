c06975cc5238640c4952b5c4b2fc56d5f231c43d
hey_DJ <dhananjoymalakar007@gmail.com>
2024-01-18 10:13:44 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import StarCanvas from '@/components/main/StarBackground';
+import Navbar from '@/components/main/Navbar';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'DJ Portfolio',
+  description: 'This is my portfolio',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+      <StarCanvas/> 
+      <Navbar />
+        {children}
+        </body>
+    </html>
+  );
+}
+
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Hero from "@/components/main/Hero";
+import Skills from '@/components/main/Skills';
+import Encryption from '@/components/main/Encryption';
+import Projects from '@/components/main/Projects';
+import Footer from '@/components/main/Footer';
+
+export default function Home() {
+  return (
+    <main className='h-full w-full'>
+      <div className='flex flex-col gap-20'>
+      <Hero />
+      <Skills />
+      <Encryption />
+      <Projects />
+      <Footer />
+    </div>
+  </main>
+  );
+}
